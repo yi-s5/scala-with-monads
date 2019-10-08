b6180d801c5c6d57a46713aea9eb89c8b364c462
Yi Fu <yi.fu@s5stratos.com>
2019-10-08 15:50:15 -0500

    fix: remove 'm'

--- src/main/scala/with_monads/Writer.scala
+++ src/main/scala/with_monads/Writer.scala
@@ -13,7 +13,7 @@ case class Writer[L: Semigroup, A](log: L , value: A) extends WriterOps[L, A] {
   def tell(entries: L): Writer[L, A]   = Writer(Semigroup.combine(log, entries), value)
 }
 
-object Writer {m
+object Writer {
   implicit def monadForWriter[L: Semigroup](implicit ev: Monoid[L]): Monad[Writer[L, *]] = new Monad[Writer[L, *]] {
     def pure[A](a: A): Writer[L, A] = Writer(Monoid.empty, a)
     def flatMap[A, B](fa: Writer[L, A])(g: A => Writer[L, B]): Writer[L, B] = 
