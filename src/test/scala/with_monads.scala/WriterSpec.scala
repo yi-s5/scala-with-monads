e46099ae3f6fc280f8bdd4bb2e37734510c55a6d
Alex Mills <alex.mills@s5stratos.com>
2019-10-10 11:32:47 -0500

    feat: add WriterT and WriterSpec test

---
+++ src/test/scala/with_monads.scala/WriterSpec.scala
@@ -0,0 +1,52 @@
+package with_monads.scala
+
+import cats.Monoid
+import cats.implicits._
+
+import with_monads._
+import with_monads.Writer
+
+import org.scalatest._
+import org.scalatest.Matchers._
+
+class WriterSpec extends FlatSpec {
+
+  case class Tree[A](value: A, left: Option[Tree[A]], right: Option[Tree[A]])
+
+  def tree[A](value: A, left: Option[Tree[A]] = None, right: Option[Tree[A]] = None): Option[Tree[A]] = {
+    Some(Tree(value, left, right))
+  }
+
+  "Writer" should "correctly log post-order tree traversals" in {
+    val t = tree("a",
+              tree("b",
+                tree("d")),
+              tree("c",
+                tree("e",
+                  tree("g"),
+                  tree("h")),
+                tree("f"))).get
+
+    def append[A,B](w: Writer[List[A],B], node: Tree[A]) = w.tell(List(node.value))
+
+    // traverse counts the leaves while storing a post-order traversal in the log
+    def traverse[A](x: Tree[A]): Option[Writer[List[A], Int]] = {
+      val maybeLeft = x.left.flatMap(traverse(_))
+      val maybeRight = x.right.flatMap(traverse(_))
+      // This could be much easier with a WriterT... why?
+      (maybeLeft, maybeRight) match {
+        case (None,    None)    => Some(append(Monad[Writer[List[A],*]].pure(1), x))
+        case (Some(l), None)    => Some(append(l, x))
+        case (None,    Some(r)) => Some(append(r, x))
+        // TODO: Fix this crap API
+        case (Some(l), Some(r)) => Some(append(l.flatMap(x => r.value(r.value + x)), x))
+      }
+    }
+
+    assert(traverse(t).isDefined)
+    val writer = traverse(t).get
+    writer.value should equal (4)
+    println(writer.log)
+    writer.log should equal(List("f", "h", "g", "e", "c", "d", "b", "a"))
+  }
+}
